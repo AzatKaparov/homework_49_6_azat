06f83dde60b6df569ddd14a01f7eca0c43828975
Azat Kaparov <azatkaparov843@gmail.com>
2020-08-06 15:16:22 +0600

    Добавление фикстуры

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Type, Task, Status
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('summary', 'status', 'type', 'created_at')
+    fields = ['summary', 'description', 'status', 'type']
+    search_fields = ('summary',)
+    list_filter = ('status',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
